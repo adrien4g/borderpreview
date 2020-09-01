f3f155b14c45aec0dc0501385a20043eb18a9f03
adrien4g <luanadriendev@gmail.com>
2020-09-01 23:25:16 +0000

    first version stable

--- scripts/script.js
+++ scripts/script.js
@@ -1,32 +1,72 @@
+const inputs = Array.from(document.getElementsByTagName("input"))
 const box = document.getElementById("box")
+const buttonReset = document.getElementById("reset")
+const textarea = document.getElementById("code")
 
-const listeners = {
-topLeft:getElement('top-left'),
-topRight:getElement('top-right'),
-bottomLeft:getElement('bottom-left'),
-bottomRight:getElement('bottom-right')}
+buttonReset.addEventListener("click", (event)=>{
+    event.target.parentElement.style.cssText = "border-radius: 0px"
+    textarea.value = ""
+    inputs.map(input=>{input.value = 0})
+})
+inputs.map(input=>{
+    input.addEventListener("input", validateInputs)
+    input.value=0
+})
 
-for (key in listeners){
-    listeners[key].addEventListener('input', changeRadius)
-    listeners[key]. addEventListener('focusin',(event)=>{event.target.value=""})
-    listeners[key]. addEventListener('focusout',(event)=>{if(event.target.value.trim()==""){return event.target.value = "0"}})
-    listeners[key].value = 0
-}
-function getElement(element){
-    return document.getElementById(element)
-}
-
-function changeRadius(event){
-    console.log(event.target.value)
-    for (key in listeners){
-        if (listeners[key].value.trim() == ""){
-            listeners[key].value = 0
-        }else if(listeners[key].value == 0){
-            listeners[key].value.substring(1);
-        }
-        
+function validateInputs(event){
+    //Verify if input.value > 360
+    //If true - input.value = 360
+    if (event.target.value > 360){
+        event.target.value = 360
     }
-    const style = `border-radius:${listeners['topLeft'].value}px ${listeners['topRight'].value}px ${listeners['bottomLeft'].value}px ${listeners['bottomRight'].value}px`
-    console.log(style)
-    box.style.cssText = style
+    //Verify if input is empty
+    //If true - input.value = 0
+    else if (event.target.value.trim() == ""){
+        event.target.value = 0
+        textarea.value = ""
+    }
+    //Verify if input.value > 0 and first letter is "0"
+    //If true - Remove first letter
+    else if(event.target.value > 0 && event.target.value.charAt(0) == 0 || event.target.value.charAt(0) == "-"){
+        event.target.value = event.target.value.substring(1)
+    }
+    changeBorderRadius()
+    generateCss()
+}
+
+function changeBorderRadius(){
+    box.style.cssText += `border-radius: ${inputs[0].value}px 
+    ${inputs[1].value}px ${inputs[3].value}px ${inputs[2].value}px`
+}
+
+function generateCss(){
+    let values = []
+    let css = ""
+    inputs.map(input=>{
+        if(input.value > 0){
+            values.push(input)
+        }
+    })
+    switch (values.length) {
+        case 1:
+            css = `border-${values[0].id}-radius:${values[0].value}px;`
+            break
+        case 2:
+            css = `border-${values[0].id}-radius:${values[0].value}px;
+border-${values[1].id}-radius:${values[1].value}px;`
+            break
+        case 3:{
+            css = `border-${values[0].id}-radius:${values[0].value}px;
+border-${values[1].id}-radius:${values[1].value}px;
+border-${values[2].id}-radius:${values[2].value}px;`
+            break
+        }
+        default:
+            css = `border-${values[0].id}-radius:${values[0].value}px;
+border-${values[1].id}-radius:${values[1].value}px;
+border-${values[2].id}-radius:${values[2].value}px;
+border-${values[3].id}-radius:${values[3].value}px;`
+            break
+    }
+    textarea.value = css;
 }
