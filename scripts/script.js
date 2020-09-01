b24ec0884ca0ab3cc40702d891d364d2a8ec7d64
adrien4g <luanadriendev@gmail.com>
2020-09-01 00:29:10 +0000

    first version stopped

--- scripts/script.js
+++ scripts/script.js
@@ -1,20 +1,32 @@
 const box = document.getElementById("box")
 
-const listeners = [    
-getElement('top-left'),
-getElement('top-right'),
-getElement('bottom-left'),
-getElement('bottom-right')]
+const listeners = {
+topLeft:getElement('top-left'),
+topRight:getElement('top-right'),
+bottomLeft:getElement('bottom-left'),
+bottomRight:getElement('bottom-right')}
 
-listeners.map(element=>{
-    element.addEventListener('input', changeRadius)
-})
+for (key in listeners){
+    listeners[key].addEventListener('input', changeRadius)
+    listeners[key]. addEventListener('focusin',(event)=>{event.target.value=""})
+    listeners[key]. addEventListener('focusout',(event)=>{if(event.target.value.trim()==""){return event.target.value = "0"}})
+    listeners[key].value = 0
+}
 function getElement(element){
     return document.getElementById(element)
 }
 
 function changeRadius(event){
     console.log(event.target.value)
-    const style = `border-${event.target.name}-radius:${event.target.value}px`
+    for (key in listeners){
+        if (listeners[key].value.trim() == ""){
+            listeners[key].value = 0
+        }else if(listeners[key].value == 0){
+            listeners[key].value.substring(1);
+        }
+        
+    }
+    const style = `border-radius:${listeners['topLeft'].value}px ${listeners['topRight'].value}px ${listeners['bottomLeft'].value}px ${listeners['bottomRight'].value}px`
+    console.log(style)
     box.style.cssText = style
 }
